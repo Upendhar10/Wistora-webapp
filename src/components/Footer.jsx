1a69c5470a89b3065449de2dab943825c998d820
upendhar Nemmani <upendharnemmani@gmail.com>
2025-03-02 18:08:27 +0530

    v0 | Footer | Created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <div className="border-t bg-gray-800 p-1 text-center text-xs italic text-white">
+      <p>
+        Designed & Developed by{" "}
+        <a
+          href="https://www.linkedin.com/in/upendharnemmani/"
+          className="hover:text-purple-500"
+          target="_blank"
+        >
+          Upendhar N
+        </a>{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
