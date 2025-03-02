59034159ac6f63c9baca78cc51ffbaa3e3c5f8fe
upendhar Nemmani <upendharnemmani@gmail.com>
2025-03-02 19:39:24 +0530

    v0 | Home-page | Created

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,8 @@
+import Home from "../pages/Home";
 const MainContainer = () => {
   return (
     <div className="border-1 m-1 h-screen w-full rounded-md border">
-      MainContainer
+      <Home />
     </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="grid h-full place-content-center gap-3 p-4 italic">
+      <h1 className="text-4xl font-bold leading-relaxed tracking-wide text-purple-500 md:text-7xl md:leading-relaxed">
+        A <span className="text-red-500">Youtube</span> inspired <br /> Video
+        Streaming Platform
+      </h1>
+      <p className="text-xl">
+        Explore multiple feature of this app using the Side Menu bar!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
