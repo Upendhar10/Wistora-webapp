e2959c2b72592f631b6e36cc6edd94eb9331b21f
upendhar Nemmani <upendharnemmani@gmail.com>
2025-03-04 19:06:04 +0530

    v0 | customMessage in LiveChat

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,16 +1,33 @@
 import { FaUserCircle } from "react-icons/fa";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addMessage } from "../reduxStore/LiveChatSlice";
 export const CustomChatMessage = () => {
+  const [myMessage, setMyMessage] = useState();
+
+  const dispatch = useDispatch();
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    dispatch(addMessage({ name: "Me", message: myMessage }));
+    setMyMessage("");
+  };
   return (
-    <div className="gap border-1 flex h-fit items-center justify-between rounded-md border">
+    <form
+      onSubmit={(e) => handleSendMessage(e)}
+      className="gap border-1 flex h-fit items-center justify-between rounded-md border"
+    >
       <input
         type="text"
         placeholder="say something......"
         className="p-2 outline-none"
+        value={myMessage}
+        onChange={(e) => setMyMessage(e.target.value)}
       />
       <button className="cursor-pointer rounded-sm bg-purple-500 p-2 text-white">
         Send
       </button>
-    </div>
+    </form>
   );
 };
 
