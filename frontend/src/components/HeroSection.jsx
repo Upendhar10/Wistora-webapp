b1f313d2e2b6f779bba20ef88254b504689e7616
Upendhar <upendharnm.dev@gmail.com>
2026-02-04 17:54:01 +0530

    v1 | {frontend) | Update Herosection

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -3,9 +3,9 @@ const HeroSection = () => {
   return (
     <section className="w-full py-20 px-6 flex flex-col items-center text-center gap-6">
       <p className="text-gray-400 max-w-3xl text-6xl font-semibold tracking-wide leading-normal">
-        A Modern Video Streaming Platform.
-      </p> <br />
-      <span className="text-gray-600 text-base text-center">focused on frontend architecture and secure API integration.</span>
+        A Modern Video Streaming Platform. <br />
+        <span className="text-gray-600 text-3xl text-center">focused on frontend architecture and secure API integration.</span>
+      </p> 
     </section>
   );
 };
