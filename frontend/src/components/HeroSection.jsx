bc44173a93507c3e0d0f5068834ffe566edc3a68
Upendhar <upendharnm.dev@gmail.com>
2026-05-04 08:33:36 +0530

    v1 | (frontend) | Update Hero Section Styling

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,9 +2,9 @@ const HeroSection = () => {
 
   return (
     <section className="w-full py-20 px-6 flex flex-col items-center text-center gap-6">
-      <p className="text-gray-400 max-w-3xl text-6xl font-semibold tracking-wide leading-normal">
-        A Modern Video Streaming Platform. <br />
-        <span className="text-gray-600 text-3xl text-center">focused on frontend architecture and secure API integration.</span>
+      <p className="text-gray-400 max-w-4xl md:text-6xl text-2xl font-semibold tracking-wide md:leading-normal italic">
+        A Modern Video Streaming Platform Inspired from YT. <br />
+        <span className="text-gray-600 md:text-2xl text-sm tracking-normal italic text-center">Built focusing on Frontend Architecture and Secure API Integration.</span>
       </p> 
     </section>
   );
