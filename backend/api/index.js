410205f168d3d65b408f6673b1fc20b778aa9895
Upendhar <upendharnm.dev@gmail.com>
2026-06-01 15:35:24 +0530

    v1 | (backend) | test express - vercel integration

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,17 +1,4 @@
 import serverless from "serverless-http";
 import app from "../src/app.js";
 
-console.log("API INDEX LOADED");
-
-const handler = serverless(app);
-
-// export default async function(req,res){
-
-//   console.log("FUNCTION INVOKED");
-
-//   return handler(req,res);
-// }
-
-export default function(req,res){
-   return res.status(200).send("VERCEL FUNCTION WORKS");
-}
+export default serverless(app);
